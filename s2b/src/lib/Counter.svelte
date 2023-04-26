0c5502cb7343647b9574c4f7f01a8bc25b1b5e8b
weakish <weakish@gmail.com>
2023-04-26 07:47:18 +0000

    :new:ui: select or input api host
    I also removed the Codeium Chat prompt,
    since some are outdated now.

--- s2b/src/lib/Counter.svelte
+++ s2b/src/lib/Counter.svelte
@@ -1,12 +1,17 @@
 <script lang="ts">
-  import { calculatePlaylistDuration, extractPlaylistId } from "./utils"
+  import { calculatePlaylistDuration, extractPlaylistId, filterApiInstance } from "./utils"
+  import { type Instance } from "./utils"
+  // https://api.invidious.io/instances.json?pretty=1
+  import instances from "./instances.json"
+  const apiHosts = filterApiInstance(instances as Instance[])
 
   let timeLength: number = 0
   let playlistId: string = ""
   let startIndex: number = 1
   let endIndex: number = 2
+  let apiHost = 'https://invidious.snopyta.org/api/v1'
   const sum = async () => {
-    timeLength = await calculatePlaylistDuration(extractPlaylistId(playlistId), startIndex - 1, endIndex - 1)
+    timeLength = await calculatePlaylistDuration(extractPlaylistId(playlistId), startIndex - 1, endIndex - 1, apiHost)
   }
 </script>
 
@@ -14,12 +19,24 @@
   <label for="playlistId">YouTube Playlist ID<sup>1</sup></label>
   <input type="text" id="playlistId" bind:value={playlistId} required>
   <br>
+
   <label for="startIndex">Start index<sup>2</sup></label>
   <input type="number" id="startIndex" bind:value={startIndex} required min=1>
   <br>
+
   <label for="endIndex">End index<sup>3</sup></label>
   <input type="number" id="endIndex" bind:value={endIndex} required min=2>
   <br>
+
+  <label for="apiHost">API host<sup>4</sup></label>
+  <input list="apiHosts" id="apiHost" bind:value={apiHost}>
+  <datalist id="apiHosts">
+    {#each apiHosts as host}
+      <option value={host} />
+    {/each}
+  </datalist>
+  <br>
+
   <button type="button" on:click={sum}>Compute total duration:</button>
   <span>
     {timeLength} sec
@@ -32,4 +49,9 @@
 <li>You can also paste the whole playlist URL, or a URL containing the <code>list</code> URL parameter.</li>
 <li>Index starts from 1, as displayed at YouTube website UI.</li>
 <li>Included.</li>
+<li>Accessing YouTubde Data API requires an API key, which is not suitable for a staic site.
+  Thus, <a href="https://docs.invidious.io/api/">Invidious API</a> is used instead.
+  You can choose from the list of available instances, or input your own.
+  Invidious is an alternative frontend to YouTube not using official YouTube API. 
+</li>
 </ol>
--- s2b/src/lib/utils.ts
+++ s2b/src/lib/utils.ts
@@ -1,20 +1,3 @@
-/* I asked Codeium Chat:
-Write a TypeScript program accepts three arguments:
-1) playlist id,
-2) the starting video index,
-3) the ending viedo index,
-and returns the total length (playtime) of all videos
-between the starting video (included) and the ending video (included).
-The individual length of the videos are fetched from quering invidious REST API.
-
-`/api/v1/playlists/:plid` already includes lengthSeconds for each video in the playlists.
-Thus there is no need to query `/api/v1/videos/:videoid` seperately.
-Besides, please use fetch API instead.
-
-In the videos array, every video has a "index" key.
-I would check that key to make sure it matches the array index
-(throws an error if mismatch is found).
-*/
 type Video = {
   id: string;
   lengthSeconds: number;
@@ -25,21 +8,11 @@ type Playlist = {
   videos: Video[];
 };
 
-/* I asked Codeium Chat:
-Currently this function uses invidious.snopyta.org as API host.
-I'd like to add an extra parameter to specify the host instead.
-*/
-/**
- * Fetches a playlist from the specified playlist ID
- * @param playlistId - The ID of the playlist to fetch
- * @param apiHost - The API host to fetch the playlist from
- * @returns - A Promise that resolves to a Playlist object
- */
 const fetchPlaylist = async (
   playlistId: string,
-  apiHost: string = 'https://invidious.snopyta.org/api/v1'
+  apiHost: string
 ): Promise<Playlist> => {
-  const response = await fetch(`${apiHost}/playlists/${playlistId}`);
+  const response = await fetch(`${apiHost}/api/v1/playlists/${playlistId}`);
   const data = await response.json();
   return data as Playlist;
 };
@@ -48,8 +21,9 @@ export const calculatePlaylistDuration = async (
   playlistId: string,
   startIndex: number,
   endIndex: number,
+  apiHost: string
 ): Promise<number> => {
-  const { videos } = await fetchPlaylist(playlistId);
+  const { videos } = await fetchPlaylist(playlistId, apiHost);
 
   let totalDuration = 0;
   for (let i = startIndex; i <= endIndex; i++) {
@@ -69,22 +43,18 @@ export const calculatePlaylistDuration = async (
 //   console.log(duration)
 // );
 
-/* I asked Codeium Chat:
-Can you write a TypeScript function which accepts a string,
-which may be a YouTube playlist ID, or a YouTube URL containing the playlist ID,
-and returns the YouTube playlistID?
-
-I do not think it is necessary to call match twice and test once.
-Also, it is unnecessary to put the whole regex in a group.
-
-I am a bit worried on corner cases for matching Playlist ID with `/PL[a-zA-Z0-9_-]+/`.
-Special playlists may not use `PL` as prefix. [0](https://stackoverflow.com/q/19795987/)
-
-Can you think of other ways to extract Playlist ID?
-*/
 export function extractPlaylistId(input: string): string | undefined {
   const url = new URL(input);
   const playlistId = url.searchParams.get('list');
 
   return playlistId;
+}
+
+type InstanceInfo = {
+  api: boolean;
+}
+export type Instance = [string, InstanceInfo]
+
+export function filterApiInstance(instances: Instance[]): string[] {
+  return instances.filter(([_, { api }]) => api).map(([host, _]) => host);
 }
